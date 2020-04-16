844c22e7481c628f9fc9f04e5bf37218995d602a
farhan mian <farhanmian099@gmial.com>
2020-04-16 17:14:57 +0530

    images are working perfect, now just need to make css file visiable in dist floder

--- webpack.config.js
+++ webpack.config.js
@@ -4,8 +4,8 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
 	entry: './src/js/script.js',
 	output: {
-		filename: 'bundle.js',
-		path: path.resolve(__dirname, 'dist')
+		path: path.resolve(__dirname, 'dist'),
+		filename: 'js/bundle.js'
 	},
 	plugins: [
 	new HtmlWebpackPlugin({
@@ -16,15 +16,28 @@ module.exports = {
 	],
 	module: {
 		rules: [
-
-		{
-			test: /\.scss$/,
-			use: [
-				'style-loader', //3) inject styles into DOM
-			 	'css-loader',   //2) turn css into comman js
-			 	'sass-loader'   //1) turn scss into css
-			 	]
-		}
+			{
+				test: /\.scss$/,
+				use: [
+					'style-loader', //3) inject styles into DOM
+				 	'css-loader',   //2) turn css into comman js
+				 	'sass-loader'   //1) turn scss into css
+				 	]
+			},
+			{
+				test: /\.html$/,
+				use: 'html-loader'
+			},
+			{
+				test: /\.png$/,
+				use: {
+					loader: 'file-loader',
+					options: {
+						name: '[name].[hash].[ext]',
+						outputPath: 'img'
+					}
+				}
+			}
 
 		]
 	}
